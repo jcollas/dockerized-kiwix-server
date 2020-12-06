c0011fe421d579f5014e9efb5caeb213cd90df45
Marcus Ulmefors <marcus@ulmefors.com>
2020-12-06 22:11:53 +0100

    fix: update kiwix-tools version

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,6 +2,9 @@
 
 # Install kiwix-tools
 # wget https://ftp.fau.de/kiwix/nightly/2017-08-13/kiwix-tools_linux64_2017-08-13.tar.gz
-wget https://ftp.fau.de/kiwix/nightly/2018-06-01/kiwix-tools_linux-x86_64-2018-06-01.tar.gz
+# wget https://ftp.fau.de/kiwix/nightly/2018-06-01/kiwix-tools_linux-x86_64-2018-06-01.tar.gz
+wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-x86_64-3.1.2-4.tar.gz
 # tar -xvzf kiwix-tools_linux-x86_64-2018-06-01.tar.gz -C ./bin
-tar -xvzf kiwix-tools_linux-x86_64-2018-06-01.tar.gz -C ./bin --strip-components 1
+# tar -xvzf kiwix-tools_linux-x86_64-2018-06-01.tar.gz -C ./bin --strip-components 1
+tar -xvzf kiwix-tools_linux-x86_64-3.1.2-4.tar.gz -C ./bin --strip-components 1
+
